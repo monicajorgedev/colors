3c4f2793f204f19305f0af2c7e0399a82d980f24
monicajorgedev <monicajorge.dev@gmail.com>
2024-11-19 11:38:27 +0100

    modificación clase

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -6,8 +6,13 @@ function BoxColor ({color, value}) {
     <>
     <li 
     className={`box ${color}`} 
-    style={value !== color ? {color: `${color}`, border: `1px solid ${color}`} : {background: `${color}`, color: 'black'}}>
-    {value !== color ? `No, I'm not a ${value} color` :`Yes, I am ${color} color` }
+    style={value !== color 
+    ? {color: `${color}`, borderColor: {color}} 
+    : {background: `${color}`, color: 'black'}}
+    >
+    {value !== color 
+    ? `No, I'm not a ${value} color` 
+    : `Yes, I am ${color} color` }
     </li>
     </>
   )
